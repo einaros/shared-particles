3affd42eacf0d06a7e781610cea59e62370d983e
einaros <einaros@gmail.com>
2011-10-19 17:29:18 +0200

    exposed plot method for easier hackability

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express.createServer();
 var io = require('socket.io').listen(app);
 app.listen(8003);
 app.use(express.static(__dirname + '/public'));
+io.set('log level', 1);
 io.sockets.on('connection', function(socket) {
     socket.on('p', function(message) {
         socket.broadcast.emit('p', message);
